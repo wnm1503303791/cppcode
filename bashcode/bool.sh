251e13a018fdded4986122f64c6b3443302cafd4
wnm1503303791 <wnm1503303791@gmail.com>
2019-08-13 12:13:14 +0800

    add shellcode

--- bashcode/bool.sh
+++ bashcode/bool.sh
@@ -2,12 +2,25 @@
 # File Name: bool.sh
 # Author: tuzhuo
 # mail: xmb028@163.com
-# Created Time: 2019年08月12日 星期一 20时23分17秒
+# Created Time: 2019年08月13日 星期二 11时19分39秒
 #########################################################################
 #!/bin/bash
-if[$1 -gt $2]
-then
-	echo $1
+if [ $1 -eq $2 ]
+then 
+	echo -e "$1 equals $2\n"
 else
-	echo $2
+	echo "$1 is not equals to $2"
 fi
+
+if [ $1 -gt $2 ]
+then
+	echo "$1 bigger than $2"
+elif [ $1 -eq $2 ]
+then
+	:
+else
+	echo "$1 smaller than $2"
+fi
+
+echo "\"test\"\n"
+echo `date`
