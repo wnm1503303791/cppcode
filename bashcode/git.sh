dcd43cc5d53fd91aa644f95f0ff88a752c6b867a
Moyu <wnm1503303791@gmail.com>
2019-10-18 19:31:14 +0800

    2019-10-18

---
+++ bashcode/git.sh
@@ -0,0 +1,5 @@
+git clone git@github.com:wnm1503303791/pycode.git
+git clone git@github.com:wnm1503303791/hello-github.git
+git clone git@github.com:wnm1503303791/basenji.git
+git clone git@github.com:wnm1503303791/cppcode.git
+git clone git@github.com:wnm1503303791/storage.git
